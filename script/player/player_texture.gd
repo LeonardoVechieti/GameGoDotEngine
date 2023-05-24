9387d37b628ef12974c25024557ae862fdce223c
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-05-24 15:06:20 -0300

    Feat: Animation player run and idle

--- script/player/player_texture.gd
+++ script/player/player_texture.gd
@@ -1,8 +1,11 @@
 extends Sprite
 class_name PlayerTexture
+ #Pega a referencia de um irmão
+export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
+	horizontal_behavior(direction)
 	print(direction)
 	
 func verify_position(direction: Vector2) -> void:
@@ -11,3 +14,8 @@ func verify_position(direction: Vector2) -> void:
 	elif direction.x < 0: 
 		flip_h = true
 
+func horizontal_behavior(direction: Vector2) -> void:
+	if direction.x != 0:
+		animation.play("run")
+	else:
+		animation.play("idle")
