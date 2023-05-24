f21f7f3a74a021bbdc89ed1870442bd125bc862d
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-05-24 15:49:52 -0300

    Feat: Creation of the logic of gravity

--- script/player/player.gd
+++ script/player/player.gd
@@ -4,11 +4,17 @@ class_name Player
 onready var player_sprite: Sprite  = get_node("Texture")
 
 var velocity: Vector2
-
+#Variaveis para ainimações
+var jump_count: int = 0
+var landing: bool = false
 export(int) var speed
+export(int) var jump_speed
+export(int) var player_gravity
 
 func _physics_process(delta: float):
 	horizontal_movement_env()
+	vertical_movement_env()
+	gravity(delta)
 	velocity = move_and_slide(velocity)
 	player_sprite.animate(velocity)
 	
@@ -17,3 +23,18 @@ func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.x = input_direction * speed
 	print(velocity.x)
+
+
+func vertical_movement_env() -> void: 
+	if is_on_floor(): 
+		jump_count = 0
+		
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+		jump_count += 1
+		velocity.y = jump_speed
+		
+	
+func gravity(delta: float) -> void:
+	velocity.y += delta * player_gravity
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
