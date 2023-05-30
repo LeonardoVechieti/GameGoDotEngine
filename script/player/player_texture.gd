212d401e86cdc7c2416d9661e28d247c28ddb3e3
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-05-30 13:45:46 -0300

    Feat: Direction animate y

--- script/player/player_texture.gd
+++ script/player/player_texture.gd
@@ -5,9 +5,19 @@ export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	horizontal_behavior(direction)
-	print(direction)
+	if direction.y != 0:
+		vertical_behavior(direction)
+	else:
+		horizontal_behavior(direction)
+	#horizontal_behavior(direction)
+	#print(direction)
 	
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		animation.play("fall")
+	elif direction.y < 0:
+		animation.play("jump")
+
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
