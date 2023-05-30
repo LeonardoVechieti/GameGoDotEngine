fcc436f7b747cba6606341601f087cee2eac281c
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-05-30 14:05:55 -0300

    Feat: Expanding the level

--- script/player/player_texture.gd
+++ script/player/player_texture.gd
@@ -2,11 +2,15 @@ extends Sprite
 class_name PlayerTexture
  #Pega a referencia de um irmão
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
+export(NodePath) onready var player =  get_node(player) as KinematicBody2D
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
 	if direction.y != 0:
 		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("landing")
+		#player.set_physics_process(false)
 	else:
 		horizontal_behavior(direction)
 	#horizontal_behavior(direction)
@@ -14,6 +18,7 @@ func animate(direction: Vector2) -> void:
 	
 func vertical_behavior(direction: Vector2) -> void:
 	if direction.y > 0:
+		player.landing = true
 		animation.play("fall")
 	elif direction.y < 0:
 		animation.play("jump")
@@ -29,3 +34,11 @@ func horizontal_behavior(direction: Vector2) -> void:
 		animation.play("run")
 	else:
 		animation.play("idle")
+
+
+func on_animation_finished(anim_name: String):
+	match anim_name:
+		"landing":
+			player.landing = false
+			#player.set_physics_process(true)
+	
